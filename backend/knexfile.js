30f8095a31506b4cabee4a3b3e3ee307b3ad10d6
SENAPATI SAI RAMYA <ramyasenapati2004@gmail.com>
2025-01-06 11:06:03 +0530

    Updated

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
+
+
 module.exports = {
-    development: {
-      client: 'mysql',
-      connection: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'project'
-      },
-      migrations: {
-        tableName: 'knex_migrations'
-      }
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+  
+    migrations: {
+      tableName: 'knex_migrations'
     }
-  };
+  }
+};
